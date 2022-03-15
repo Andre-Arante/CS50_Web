25f8723fe498c5dc38803a39d1bcb03494595593
Andre-Arante <andrearante12@gmail.com>
2022-03-14 18:55:02 -0700

    might add img support later on

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,18 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Auction_Listings(models.Model):
+    """
+    Model to control auction listings
+
+    User can view listings via view_listings or active_listings
+    edit via create_listings
+    """
+
+    name = models.CharField(max_length=64)
+    selling_price = models.IntegerField(default=100)
+    image = models.ImageField()
+
+    def __str__(self):
+        return f"{self.name}: ${self.selling_price}. Image at {self.image}"
