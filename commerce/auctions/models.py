4652a84c344839040c89172c72664b8dd37db541
Andre-Arante <andrearante12@gmail.com>
2022-03-14 20:34:40 -0700

    images finally work - eaz dubs

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
+from django.utils import timezone
 
 class User(AbstractUser):
     pass
@@ -15,7 +15,10 @@ class Auction_Listings(models.Model):
 
     name = models.CharField(max_length=64)
     selling_price = models.IntegerField(default=100)
-    image = models.ImageField()
+    image = models.ImageField(upload_to='auctions\static', default='..auctions/static/default.jpg')
+
+    def date_published():
+        return self.date.strftime('%B %d %Y')
 
     def __str__(self):
-        return f"{self.name}: ${self.selling_price}. Image at {self.image}"
+        return f"{self.name} for ${self.selling_price}. Image can be found at {self.image}"
